61e52c39bb18761fc101065f3069454c41c3f278
Abdallah-Mohamed-SE <abdallahmohamedabdelhay1@gmail.com>
2026-07-01 14:58:47 +0300

    Modify print statement to use single quotes
    Updated print statement to use single quotes for the string.

--- Python/Week 1/task 4.py
+++ Python/Week 1/task 4.py
@@ -1,4 +1,4 @@
 name ="abdallah"
 age = "20"
 country = "Egypt"
-print("Hello, My Name Is "+name+" And Iam "+age+" Years Old and I Live in "+country+".")
+print('"Hello, My Name Is "+name+" And Iam "+age+" Years Old and I Live in "+country+"."') #string inside single qoutes
